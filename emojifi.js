1d08915dc3a43982434a6ac84255ee9e8069b2a5
Owen Hurford <owen@hurfo.com>
2015-05-16 16:03:11 +0100

    Recursively Emojifi so only nodes containing emoji are updated

--- emojifi.js
+++ emojifi.js
@@ -7,9 +7,33 @@ emojione.imagePathSVG = chrome.extension.getURL('emojione/assets/svg/');
 emojione.imagePathPNG = chrome.extension.getURL('emojione/assets/png/');
 
 function emojifi() {
-    var original = window.document.getElementsByTagName('body')[0].innerHTML;
-    var modified = emojione.toImage(original);
-    if(original != modified) window.document.getElementsByTagName('body')[0].innerHTML = modified;
+    recursiveEmojifi(window.document.body);
+}
+
+function recursiveEmojifi(node) {
+    var i;
+    
+    if (node) {
+        if (node.hasChildNodes()) {
+            for (i = 0; i < node.childNodes.length; i++) {
+                recursiveEmojifi(node.childNodes[i]);
+            }
+        } else {
+            var original = node.textContent, modified = emojione.toImage(original);
+            if (original !== modified && (node.nodeType == Node.TEXT_NODE || node.nodeType == Node.ELEMENT_NODE)) {
+                parser = new DOMParser();
+                modified = parser.parseFromString(modified, 'text/html');
+                if(modified.body.childNodes.length > 1) {
+                    for(i = 0; i < modified.body.childNodes.length; i++) {
+                        node.parentNode.insertBefore(modified.body.childNodes[i], node);
+                    }
+                    node.parentNode.removeChild(node);
+                } else {
+                    node.parentNode.replaceChild(modified.body.firstChild, node);
+                }
+            }
+        }
+    }
 }
 
 emojifi();
